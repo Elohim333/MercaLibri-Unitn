a19d3bdb8ff51519347c5570afe302f3981ea927
mario <95216276+francescoboldrin@users.noreply.github.com>
2021-12-17 16:59:51 +0100

    sviluppo front-end

--- ui/catalogo.js
+++ ui/catalogo.js
@@ -161,103 +161,79 @@ Vue.component('product', {
       }
   })*/
 
+  var text1="Titolo";
+  var text2="Prezzo";
+  var text3 = "Valutazione";
+  var message= " ";
+
 
   const catalogo = {
     template: `
     <div>
+    <div>
+    <button type="button"
+    class="btn btn-primary m-2 fload-end"
+    @click="Ricerca(message)">
+     Search
+    </button>
+    <input v-model="message" placeholder="edit me" >
+    
 
-<button type="button"
-class="btn btn-primary m-2 fload-end"
-data-bs-toggle="modal"
-data-bs-target="#exampleModal"
-@click="addClick()">
- Add Employee
-</button>
+    </div>
 
 <table class="table table-striped">
 <thead>
     <tr>
         <th>
-            Employee Id
+            Titolo
         </th>
         <th>
-            Employee Name
+            Venditore
         </th>
         <th>
-            Department
+            Prezzo
         </th>
         <th>
-            DOJ
-        </th>
-        <th>
-            Options
+            Valutazione
         </th>
     </tr>
 </thead>
 <tbody>
-    <tr v-for="ilb in libri">
+    <tr v-for="lib in libri">
         <td>{{lib.Titolo}}</td>
         <td>{{lib.Venditore}}</td>
         <td>{{lib.Prezzo}}</td>
         <td>{{lib.Valutazione}}</td>
-        <td>
-            <button type="button"
-            class="btn btn-light mr-1"
-            data-bs-toggle="modal"
-            data-bs-target="#exampleModal"
-            @click="editClick(lib)">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                </svg>
-            </button>
-            <button type="button" @click="deleteClick(lib.Titolo)"
-            class="btn btn-light mr-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                </svg>
-            </button>
-
-        </td>
     </tr>
 </tbody>
 </thead>
 </table>
-
-<div class="modal fade" id="exampleModal" tabindex="-1"
-    aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog modal-lg modal-dialog-centered">
-<div class="modal-content">
-    <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">{{modalTitle}}</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal"
-        aria-label="Close"></button>
+<div>
+    <button type="button"
+    class="btn btn-primary m-2 fload-end"
+    @click="Filtro('Titolo')">
+     Filtro per Titolo
+    </button>
+    <button type="button"
+    class="btn btn-primary m-2 fload-end"
+    @click="Filtro('Valutazione')">
+     Filtro per Valutazione
+    </button>
+    <button type="button"
+    class="btn btn-primary m-2 fload-end"
+    @click="Filtro('Prezzo')">
+     Filtro per Prezzo
+    </button>
+    <button type="button"
+    class="btn btn-primary m-2 fload-end"
+    @click="Filtro('default')">
+     Default
+    </button>
     </div>
 
-    <div class="modal-body">
-    <div class="d-flex flex-row bd-highlight mb-3">
-        <div class="p-2 w-50 bd-highlight">
-            <div class="input-group mb-3">
-                <span class="input-group-text">Name</span>
-                <input type="text" class="form-control" v-model="EmployeeName">
-            </div>
-
-            <div class="input-group mb-3">
-                <span class="input-group-text">DOJ</span>
-                <input type="date" class="form-control" v-model="DateOfJoining">
-            </div>
-
-        </div>
-
-</div>
-</div>
-</div>
 
 
 </div>
-
-
-  </div>
   `,
   
     data() {
@@ -278,9 +254,9 @@ data-bs-target="#exampleModal"
     },
     methods: {
       fetchData() {
-        axios.get(variables.API_URL + 'libri/')
+        axios.get(variables.API_URL + "libri")
           .then((response) => {
-            this.libri = JSON.parse(response.data);
+            this.libri = response.data;
           });
       },
       addClick() {
@@ -310,6 +286,18 @@ data-bs-target="#exampleModal"
         document.querySelector('#addEmployee').className = 'btn btn-light px-2 py-0';
         document.querySelector('#map').className = 'd-none';
       }*/
+      Filtro(params) {
+        axios.get(variables.API_URL + "libri/filtro/"+params)
+        .then((response) => {
+          this.libri = response.data;
+        });
+      },
+      Ricerca(params){
+        axios.get(variables.API_URL + "libri/search/"+params)
+        .then((response) => {
+          this.libri = response.data;
+        });
+      }
     },
     mounted: function () {
       this.fetchData();
